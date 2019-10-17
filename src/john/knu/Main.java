c4b1f3fe223ce135ce542a03748f4861e7020efd
john <john@gmail.com>
2019-10-17 15:02:50 +0800

    修改port編號

--- src/john/knu/Main.java
+++ src/john/knu/Main.java
@@ -8,13 +8,13 @@ import java.net.SocketException;
 public class Main {
     public static void main(String[] args) {
         try {
-            DatagramSocket ds = new DatagramSocket(8000);
+            DatagramSocket ds = new DatagramSocket(6000);
             byte[] buf = new byte[1024];
             DatagramPacket dp = new DatagramPacket(buf, buf.length);
             do {
                 ds.receive(dp);
                 String s = new String(dp.getData(), 0, dp.getLength());
-                System.out.println("收到：" + s);
+                System.out.println("已收到：" + s);
             } while (true);
         } catch (SocketException e) {
             e.printStackTrace();
